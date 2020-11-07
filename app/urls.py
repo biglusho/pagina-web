ba5c653007f7386b6cd33d95779439f15f6b4369
luis aravena <lu.aravenab@alumnos.duoc.cl>
2020-11-07 20:54:22 -0300

    pagina web ponelobien

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from .views import contacto
+from .views import terminos_condiciones
+from .views import registro
 
 urlpatterns = [
     path('', home, name="home"),
+    path('contacto/', contacto, name="contacto"),
+    path('terminosytondiciones/', terminos_condiciones, name="terminosYcondiciones"),
+    path('registro/', registro, name="registro"),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/home.html')
+
+def contacto(request):
+    return render(request, 'app/contacto.html')
+
+def terminos_condiciones(request):
+    return render(request, 'app/terminos-condiciones.html')
+
+def registro(request):
+    return render(request, 'app/registro.html')
